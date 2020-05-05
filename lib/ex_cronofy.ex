2ca42537054f2b6361c81e9ceb32929d8ac24c28
Edwin Mak <edwinthinks@gmail.com>
2020-05-05 18:02:42 -0500

    Init commit

---
+++ lib/ex_cronofy.ex
@@ -0,0 +1,18 @@
+defmodule ExCronofy do
+  @moduledoc """
+  Documentation for `ExCronofy`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExCronofy.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_cronofy_test.exs
@@ -0,0 +1,8 @@
+defmodule ExCronofyTest do
+  use ExUnit.Case
+  doctest ExCronofy
+
+  test "greets the world" do
+    assert ExCronofy.hello() == :world
+  end
+end

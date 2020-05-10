6b78fcc4522f328091a31a99e2090b52f5017046
Edwin Mak <edwinthinks@gmail.com>
2020-05-10 15:20:53 -0500

    Add create_or_update_event function

--- lib/ex_cronofy/events.ex
+++ lib/ex_cronofy/events.ex
@@ -6,7 +6,7 @@ defmodule ExCronofy.Events do
   alias ExCronofy.ApiClient
 
   @doc """
-  Sends a request to get a list the calendars for a authenticated user
+  Gets a list of events across calendars for a authenticated user
 
   ## Parameters
 
@@ -24,6 +24,40 @@ defmodule ExCronofy.Events do
     path = "/v1/events?" <> URI.encode_query(query_params)
 
     ApiClient.get(path, [
+      {"Authorization", "Bearer #{access_token}"}
+    ])
+  end
+
+  @doc """
+  Creates or updates a event on a calendar_id
+
+  ## Parameters
+
+    - calendar_id: an id of the calendar
+    - event_attrs: details about the event to be created or updated
+    - access_token: a access token
+
+  ## Examples
+      
+      iex> calendar_id = "wibble"
+      iex> event_attrs = %{
+        description: "Discuss plans for the next quarter.",
+        end: "2020-05-12T17:00:00Z",
+        event_id: "qTtZdczOccgaPncGJaCiLg",
+        location: %{
+          description: "Board room"
+        },
+        start: "2020-05-12T15:30:00Z",
+        summary: "Board meeting"
+      }
+      iex> ExCronofy.Events.create_or_update_event(calendar_id, event_attrs, "random_access_token")
+
+  """
+  @spec create_or_update_event(String.t(), map, String.t()) :: tuple
+  def create_or_update_event(calendar_id, event_attrs, access_token) do
+    path = "/v1/calendars/#{calendar_id}/events"
+
+    ApiClient.post(path, event_attrs, [
       {"Authorization", "Bearer #{access_token}"}
     ])
   end
--- test/ex_cronofy/events_test.exs
+++ test/ex_cronofy/events_test.exs
@@ -35,4 +35,40 @@ defmodule ExCronofy.EventsTest do
       end
     end
   end
+
+  describe "create_or_update_event/2" do
+    # test "raises FunctionClauseError when tzid is not provided in query_params" do
+    # access_token = Faker.String.base64()
+
+    # invalid_query_params = %{}
+
+    # assert_raise FunctionClauseError, fn ->
+    # Events.read_events(invalid_query_params, access_token)
+    # end
+    # end
+
+    test "sends API request to properly with query params and returns ok with response" do
+      access_token = Faker.String.base64()
+      calendar_id = Faker.String.base64()
+
+      path = "/v1/calendars/#{calendar_id}/events"
+
+      event_attrs = %{
+        description: "Discuss plans for the next quarter.",
+        end: "2020-05-12T17:00:00Z",
+        event_id: "qTtZdczOccgaPncGJaCiLg",
+        location: %{description: "Board room"},
+        start: "2020-05-12T15:30:00Z",
+        summary: "Board meeting"
+      }
+
+      fake_response = Faker.String.base64()
+      headers = [{"Authorization", "Bearer #{access_token}"}]
+
+      with_mock ApiClient, post: fn ^path, ^event_attrs, ^headers -> {:ok, fake_response} end do
+        assert {:ok, fake_response} ==
+                 Events.create_or_update_event(calendar_id, event_attrs, access_token)
+      end
+    end
+  end
 end
